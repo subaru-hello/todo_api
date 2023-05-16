4438440a3d500d72453cea9c6f9c537333b6e9a5
subaru-hello <octosubaru0926@gmail.com>
2023-05-16 22:44:22 +0900

    debug用origin追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://rails-next-heroku-lwuoc6jsw-subaru-hello.vercel.app/'
+    origins 'https://rails-next-heroku-lwuoc6jsw-subaru-hello.vercel.app', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
