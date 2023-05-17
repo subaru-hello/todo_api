3d28f0b296f1557d37eb84b380f3b0ab236a695c
subaru-hello <octosubaru0926@gmail.com>
2023-05-17 22:14:53 +0900

    create destroyを追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,17 +1,40 @@
+# frozen_string_literal: true
+
 class TodosController < ApplicationController
-    def index
-        todos = Todo.all
-        render json: todos
-    end
+  def index
+    todos = Todo.all
+    render json: todos
+  end
 
-    def show
-        todo = Todo.find(params[:id])
-        render json: todo
-    end
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo
+  end
 
-    private
+  def create
+    todo = Todo.new(todo_params)
 
-    def todo_params
-        params.require(:todo).permit(:id, :title, :body)
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: todo.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+
+    if todo.destroy
+      render json: { status: 'SUCCESS', message: 'Deleted the todo', data: todo }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Todo not deleted', data: todo.errors },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :contents)
+  end
 end
